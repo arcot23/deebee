3845080cbfa2bfece52750a54a43efd3991d3f45
Prabhuram Venkatesan <prabhuram.v@gmail.com>
2017-05-04 19:40:16 -0400

    compare.R renamed to reconresults.R

---
+++ R/reconresults.R
@@ -0,0 +1,36 @@
+#' @title Compares two SQL resultsets from two database environments
+#' @description Takes a SQL statement as an input and executes the SQL in two database environments, then compares the SQL resultset for differences.
+#'
+#' @param query SQL Query.
+#' @param env1 Environment string to the first database.
+#' @param env2 Environment string to the second database.
+#' @return Returns a tibble of differences
+#'
+CompareOraDataset <-
+  function(query,
+           env1,
+           env2) {
+    x <- OraRun(query, env1)
+    y <- OraRun(query, env2)
+    if (length(x) != length(y))
+      warning("Column count between x and y are not the same")
+    if (!identical(colnames(x), colnames(y)))     
+      warning("Column names between x and y are not the same")   
+    if (!identical(sapply(x, class), sapply(y, class)))     
+      warning("Column types between x and y are not the same")       
+
+    x$x = T   
+    y$y = T   
+    z <- merge(x, y, all = T) %>%     
+      as_data_frame()       
+
+    cat(sprintf(
+      "# x: %s, y: %s, xuy: %s, x-y: %s, y-x: %s;",
+      nrow(x),
+      nrow(y),
+      nrow(z),
+      nrow(z[z$y != T,]),
+      nrow(z[z$x != T,])
+    ))       
+    z
+  }
