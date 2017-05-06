06860fd556f85cc2ab0c945386c64c3e1bb8543e
Prabhuram Venkatesan <prabhuram.v@gmail.com>
2017-05-05 21:14:57 -0400

    renamed function

--- R/reconresults.R
+++ R/reconresults.R
@@ -6,7 +6,7 @@
 #' @param env2 Environment string to the second database.
 #' @return Returns a tibble of differences
 #'
-CompareOraDataset <-
+ReconResults <-
   function(query,
            env1,
            env2) {
@@ -14,23 +14,23 @@ CompareOraDataset <-
     y <- OraRun(query, env2)
     if (length(x) != length(y))
       warning("Column count between x and y are not the same")
-    if (!identical(colnames(x), colnames(y)))     
-      warning("Column names between x and y are not the same")   
-    if (!identical(sapply(x, class), sapply(y, class)))     
-      warning("Column types between x and y are not the same")       
+    if (!identical(colnames(x), colnames(y)))
+      warning("Column names between x and y are not the same")
+    if (!identical(sapply(x, class), sapply(y, class)))
+      warning("Column types between x and y are not the same")
 
-    x$x = T   
-    y$y = T   
-    z <- merge(x, y, all = T) %>%     
-      as_data_frame()       
+    x$x = T
+    y$y = T
+    z <- merge(x, y, all = T) %>%
+      as_data_frame()
 
     cat(sprintf(
-      "# x: %s, y: %s, xUy: %s, x-y: %s, y-x: %s;",
+      "# x : %s, y:%s, x \U22C3 y : %s, x - y : %s, y - x : %s\r\n",
       nrow(x),
       nrow(y),
       nrow(z),
-      nrow(z[z$y != T,]),
-      nrow(z[z$x != T,])
-    ))       
+      nrow(z[z$y != T, ]),
+      nrow(z[z$x != T, ])
+    ))
     z
   }
